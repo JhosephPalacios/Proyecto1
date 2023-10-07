541664413d5c1d58bac318d5983c99bd4fa67ee0
Fabricio Gutiérrez <78441105+FabriGR@users.noreply.github.com>
2023-10-07 23:33:11 +0000

    save.1

--- src/pages/pagina1.js
+++ src/pages/pagina1.js
@@ -1,14 +1,13 @@
 import Link from "next/link"
 
-const Index = () => {
-    return ( <
-        div >
-        <
-        h1 > Home Page o Welcome Page < /h1> <
-        h1 > Home < /h1> <
-        Link href = "pagina2" > Ir a la pagina2 < /Link> <
-        Link href = "modificar_p_al" > MI PRUEBA GIT < /Link> <
-        /div>
+const Index  = () => {
+    return (
+    <div>
+        <h1>Home Page o Welcome Page</h1>
+        <h1>Home</h1>
+        <Link href="pagina2">Ir a la pagina2</Link>
+        <Link href="modificar_p_al">MI PRUEBA GIT</Link>
+    </div>
     )
 }
 
