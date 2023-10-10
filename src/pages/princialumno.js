6185afc9ee71131e4eb2df896d3c7853d71371d1
ruben-arm14 <rubenqc_1405@outlook.com>
2023-10-10 16:47:11 -0500

    modificacion alumno

--- src/pages/princialumno.js
+++ src/pages/princialumno.js
@@ -1,55 +1,84 @@
 import React, { useEffect } from 'react';
-import '../styles/princialumn.css';
-
+import '../styles/princialumn.css'; // Asegúrate de importar tu hoja de estilos
 function App() {
+  const user = 'NombreDeUsuario'; // Debes definir el nombre de usuario
+
   useEffect(() => {
     const menuIcon = document.getElementById('menuIcon');
     const sidebar = document.getElementById('sidebar');
 
-    menuIcon.addEventListener('click', function () {
-      sidebar.classList.toggle('active');
-    });
+    if (menuIcon && sidebar) {
+      menuIcon.addEventListener('click', function () {
+        sidebar.classList.toggle('active');
+      });
+    }
   }, []);
+
   return (
-    <div className="App">
-      <div className="header">
-        <div className="logo" id="menuIcon">
-          <img src="menu.png" alt="Icono Izquierdo" className="imagen2" />
-        </div>
-        <img src="usuario.png" alt="Icono Derecho" className="imagen" />
-      </div>
-      <div className="container">
-        <div className="sidebar" id="sidebar">
-          <ul className="menu">
-            <li>Inicio</li>
-            <li>Perfil</li>
-            <li>Bibliotecas</li>
-          </ul>
-          <p>Biblio v1.0.1-alpha</p>
-        </div>
-        <div className="content">
-          <div className="welcome">Bienvenido, {"{user}"}</div>
-          <div className="divider"></div>
-          <div className="box">
-            <div className="text-box">
-              <p>Últimas reservas</p>
-            </div>
-            <div className="new-box"></div>
-            <div className="new-box2"></div>
-            <div className="inner-box"></div>
-            <div className="inner-box2"> </div>
+    <div>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Mi Página</title>
+        <link rel="stylesheet" href="pagiusu.css" />
+      </head>
+      <body>
+        <div className="header">
+          <div className="logo" id="menuIcon">
+            <img src="menu.png" alt="Icono Izquierdo" className="imagen2" />
           </div>
-          <div className="box">
-            <div className="text-box">
-              <p>Los más pedidos</p>
+          <img src="usuario.png" alt="Icono Derecho" className="imagen" />
+        </div>
+        <div className="container">
+          <div className="sidebar" id="sidebar">
+            <ul className="menu">
+              <li>Principal</li>
+              <li>Perfil</li>
+              <li>Préstamos</li>
+            </ul>
+            <p>SAC v1.0.1-alpha</p>
+          </div>
+          <div className="content">
+            <div className="welcome">Bienvenido, {user}</div>
+            <div className="divider"></div>
+            <div className="box box1">
+              <div className="text-box">
+                <p>Últimas reservas</p>
+              </div>
+              <div className="new-box">
+                <div className="circle"></div>
+                <div className="text">"Clean Code: A Handbook of Agile Software Craftsmanship"</div>
+                <div className="text2">18/09/2023 08:00 am</div>
+                <img src="portada.png" alt="Imagen" className="image" />
+              </div>
+              <div className="new-box3">
+                <div className="circle"></div>
+                <div className="text"></div>
+                <div className="text2">18/09/2023 08:00 am</div>
+                <img src="portada.png" alt="Imagen" className="image" />
+              </div>
             </div>
-            <div className="new-box"></div>
-            <div className="new-box2"></div>
-            <div className="inner-box"></div>
-            <div className="inner-box2"></div>
+            
+            <div className="box box2">
+              <div className="text-box">
+                <p>Los más pedidos</p>
+              </div>
+              <div className="new-box4">
+                <div className="circle"></div>
+                <div className="text">"Clean Code: A Handbook of Agile Software Craftsmanship"</div>
+                <div className="text2">18/09/2023 08:00 am</div>
+                <img src="portada.png" alt="Imagen" className="image" />
+              </div>
+              <div className="new-box2">
+                <div className="circle"></div>
+                <div className="text"></div>
+                <div className="text2">18/09/2023 08:00 am</div>
+                <img src="portada.png" alt="Imagen" className="image" />
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </body>
     </div>
   );
 }
