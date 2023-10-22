5594fdf89a1abf58b3b6143f127ba6c28d6bce81
ruben-arm14 <rubenqc_1405@outlook.com>
2023-10-21 23:49:36 -0500

    Cambio

--- next.config.js
+++ next.config.js
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns : [
+            {
+                protocol : 'https',
+                hostname : 'images.cdn1.buscalibre.com',
+                port: '',
+                pathname : '/**',
+            },
+            {
+                protocol : 'https',
+                hostname : 'images.cdn3.buscalibre.com',
+                port: '',
+                pathname : '/**',
+            },
+            {
+                protocol : 'https',
+                hostname : 'images.cdn2.buscalibre.com',
+                port: '',
+                pathname : '/**',
+            },
+            {
+                protocol : 'http',
+                hostname : 'dummyimage.com',
+                port: '',
+                pathname : '/**',
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
